17c57beea4ec8965b2abffd075a737d434151e07
Viniciusdev34 <160533398+Viniciusdev34@users.noreply.github.com>
2024-11-18 10:55:18 -0300

    commit second

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 const decreaseButton = document.getElementById("decrease");
 const increaseButton = document.getElementById("increase");
 const quantityDisplay = document.getElementById("quantity");
-
+const menuIcon = document.querySelector(".menu-icon");
+function toggleMenu(){
+  let menu = document.querySelector(".navbar");
+  if(menu.classList.contains('open')){
+    menu.classList.remove("open")
+  }else{
+    menu.classList.add("open")
+  }
+}
+menuIcon.addEventListener("click",toggleMenu)
 let quantity = 1;
 
 decreaseButton.addEventListener("click", () => {
